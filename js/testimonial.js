337093d8026d7a5e91db5463e5c7497e98dd83fb
Kiran Chaudhary <chaudharyk456@gmail.com>
2022-05-30 15:21:52 +0545

    Homepage completed, added missing links

--- js/testimonial.js
+++ js/testimonial.js
@@ -1,30 +1,3 @@
-
-// var slider = tns({
-//     container: ".my-slider-2",
-//     items: 5,
-//     gutter: 20,
-//     slideBy: 1,
-//     mouseDrag: true,
-//     autoplay: true,
-//     autoplayButtonOutput: false,
-//     // mode: 'gallery',
-//     speed: 1500,
-//     // animateIn: "scale",
-//     // controls: false,
-//     controlsContainer: "#custom-control-2",
-//     nav: false,
-//     // edgePadding: 20,
-//     // loop: false,
-//   });
-
-
-// new Glide('.glide', {
-//   type: 'carousel',
-//   startAt: 0,
-//   perView: 3,
-//   // gap: 40,
-//   focusAt: 'center',
-// }).mount()
 let current_comment_index = 0;
 let numb = document.getElementById("all-comments").children.length;
 
