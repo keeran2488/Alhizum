307dbdd9ccadea1aeaa16f54472483b1f23b32a7
Kiran Chaudhary <chaudharyk456@gmail.com>
2023-07-03 22:59:32 +0545

    removed console logs

--- js/testimonial.js
+++ js/testimonial.js
@@ -6,9 +6,6 @@ function nextComment() {
 
   // current_comment_index = (current_comment_index + 1)%numb
   next_comment_index = (current_comment_index + 1)%numb
-  console.log("current:"+current_comment_index)
-  console.log("Next index:"+ next_comment_index)
-
   
   next_comment = document.getElementById('comment-'+next_comment_index)
   comment.classList.toggle('hidden')
@@ -21,17 +18,12 @@ function nextComment() {
 function prevComment() {
   comment = document.getElementById('comment-' + Math.abs(current_comment_index))
   comment.classList.toggle('hidden')
-  
-  console.log("current:"+ current_comment_index)
 
   if (current_comment_index == 0){
     current_comment_index = numb
   }
   prev_comment_index = (current_comment_index - 1)%numb
   prev_comment_index = (Math.abs(prev_comment_index))
-
-  
-  console.log("prev index:"+ prev_comment_index)
 
   prev_comment = document.getElementById('comment-' + prev_comment_index)
   
